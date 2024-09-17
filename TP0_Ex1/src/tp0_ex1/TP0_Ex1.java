8395f78ba9992c0fc7945624d65b9ff6a26f7d8c
Constance FABRE <votre_mail>
2024-09-17 16:42:03 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Ex1/src/tp0_ex1/TP0_Ex1.java
@@ -0,0 +1,32 @@
+/*
+*ConstanceFABRE
+ *TDC
+ * TP0 Exercice 1
+ * 17 septembre 2024 
+ */
+package tp0_ex1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author constancefabre
+ */
+public class TP0_Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           
+        System.out.println("Bonjour");
+        System.out.println("Au-revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in); 
+        System.out.println("Bonjour, quel est votre prenom ?"); 
+        prenom = sc.nextLine();
+                
+    }
+    
+}
