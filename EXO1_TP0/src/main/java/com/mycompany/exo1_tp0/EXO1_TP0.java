3593befb5a3cdbaf43428bfb983765ad86038977
Constance FABRE <votre_mail>
2024-09-20 12:10:07 +0200

    Création d'un nouveau projet versionné

---
+++ EXO1_TP0/src/main/java/com/mycompany/exo1_tp0/EXO1_TP0.java
@@ -0,0 +1,25 @@
+/*
+ * ConstanceFABRE
+ * TP0 exercice 1
+ */
+
+package com.mycompany.exo1_tp0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author constancefabre
+ */
+public class EXO1_TP0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Bonjour");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in); 
+        System.out.println("Bonjour, quel est votre prenom ?"); 
+        prenom = sc.nextLine();
+    }
+}
