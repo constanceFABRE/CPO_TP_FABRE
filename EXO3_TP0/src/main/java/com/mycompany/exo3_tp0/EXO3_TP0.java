fdca63e2e8896059cd9f50b2f5913f6328f086aa
Constance FABRE <votre_mail>
2024-09-20 12:55:33 +0200

    Création d'un nouveau projet versionné

---
+++ EXO3_TP0/src/main/java/com/mycompany/exo3_tp0/EXO3_TP0.java
@@ -0,0 +1,29 @@
+/*
+* Constance FABRE
+Exercice 3 TP0
+Vendredi 20 septembre 2024
+ */
+
+package com.mycompany.exo3_tp0;
+
+/**
+ *
+ * @author constancefabre
+ */
+public class EXO3_TP0 {
+
+    public static void main(String[] args) {
+        System.out.println("please enter the operator :");
+        System.out.println("1) Add ");
+        System.out.println("2) substract ");
+        System.out.println("3) multiply ");
+        System.out.println("4) divide ");
+        System.out.println("5) modulo ");
+        int operator;
+        int operande1;
+        int operande2;
+        System.out.println("veuillez entrer un nombre ");
+        System.out.println("veuillez entrer un nombre ");
+        
+    }
+}
