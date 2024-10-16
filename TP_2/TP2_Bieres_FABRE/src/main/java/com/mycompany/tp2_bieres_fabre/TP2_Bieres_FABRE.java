dcf7820829bc86fabcdcaa829000da0be6a1e8c4
Constance FABRE <votre_mail>
2024-10-16 12:57:59 +0200

    Création d'un nouveau projet versionné

---
+++ TP_2/TP2_Bieres_FABRE/src/main/java/com/mycompany/tp2_bieres_fabre/TP2_Bieres_FABRE.java
@@ -0,0 +1,34 @@
+/*
+ * ConstanceFABRE TDC
+* TP2 Ex 1
+* 16 Octobre 2024
+*/
+
+package com.mycompany.tp2_bieres_fabre;
+
+/**
+ *
+ * @author constancefabre
+ */
+public class TP2_Bieres_FABRE {
+
+    public static void main(String[] args) {
+        
+      //BouteilleBiere uneBiere = new BouteilleBiere() ;  
+      //uneBiere.nom = "Cuvée des trolls";
+     // uneBiere.degreAlcool = 7.0; 
+      //uneBiere.brasserie = "Dubuisson" ;
+      //uneBiere.ouverte = false ;
+      //uneBiere.lireEtiquette();
+      
+      //BouteilleBiere secondeBiere = new BouteilleBiere() ;  
+      //secondeBiere.nom = " Leffe ";
+      //secondeBiere.degreAlcool = 6.6; 
+      //secondeBiere.brasserie = "Abbaye de Leffe" ;
+      //secondeBiere.ouverte = false ;
+      //secondeBiere.lireEtiquette();
+      
+      
+        
+    }
+}
