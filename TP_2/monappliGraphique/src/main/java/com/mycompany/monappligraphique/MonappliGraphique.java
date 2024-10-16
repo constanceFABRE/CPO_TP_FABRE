145030487c7878dc0c622f467fb51b913c552ad1
Constance FABRE <votre_mail>
2024-10-16 12:29:41 +0200

    Création d'un nouveau projet versionné

---
+++ TP_2/monappliGraphique/src/main/java/com/mycompany/monappligraphique/MonappliGraphique.java
@@ -0,0 +1,21 @@
+/*
+ *  ConstanceFABRE TDC
+* 16 Octobre 2024
+*/
+
+
+package com.mycompany.monappligraphique;
+
+import com.mycompany.monappligraphique.fenetre1;
+
+/**
+ *
+ * @author constancefabre
+ */
+public class MonappliGraphique {
+
+    public static void main(String[] args) {
+        fenetre1 f = new fenetre1();
+        f.show();
+    }
+}
