b9019c97065cc0302c964725f32a2c589b487e25
Constance FABRE <votre_mail>
2024-10-23 11:51:53 +0200

    Création d'un nouveau projet versionné

--- TP_2/TP2_Bieres_FABRE/src/main/java/com/mycompany/tp2_bieres_fabre/BouteilleBiere.java
+++ TP_2/TP2_Bieres_FABRE/src/main/java/com/mycompany/tp2_bieres_fabre/BouteilleBiere.java
@@ -14,16 +14,36 @@ public class BouteilleBiere {
     String brasserie;
     double degreAlcool;
     boolean ouverte ;
-    
+   
     public void lireEtiquette() {
-         System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) ");
+        System.out.println("Brasserie : " + brasserie ) ;
     }
-    
+   
     public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
         nom = unNom;
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
         ouverte = false;
     }
-    
+   
+    public void decapsuler() {
+        if (ouverte == false) {
+            ouverte = true;
+        }
+        else if (ouverte == true) {
+            System.out.println("erreur : biere deja ouverte");
+            ouverte = false;
+        }
+        System.out.println(ouverte);
+    }
+   
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres) Ouverte ? ";
+        if (ouverte == true ) chaine_a_retourner += "oui" ;
+        else chaine_a_retourner += "non" ;
+        return chaine_a_retourner;
+    }
 }
--- TP_2/TP2_Bieres_FABRE/src/main/java/com/mycompany/tp2_bieres_fabre/TP2_Bieres_FABRE.java
+++ TP_2/TP2_Bieres_FABRE/src/main/java/com/mycompany/tp2_bieres_fabre/TP2_Bieres_FABRE.java
@@ -13,22 +13,41 @@ package com.mycompany.tp2_bieres_fabre;
 public class TP2_Bieres_FABRE {
 
     public static void main(String[] args) {
-        
-      //BouteilleBiere uneBiere = new BouteilleBiere() ;  
-      //uneBiere.nom = "Cuvée des trolls";
-     // uneBiere.degreAlcool = 7.0; 
-      //uneBiere.brasserie = "Dubuisson" ;
-      //uneBiere.ouverte = false ;
-      //uneBiere.lireEtiquette();
-      
-      //BouteilleBiere secondeBiere = new BouteilleBiere() ;  
-      //secondeBiere.nom = " Leffe ";
-      //secondeBiere.degreAlcool = 6.6; 
-      //secondeBiere.brasserie = "Abbaye de Leffe" ;
-      //secondeBiere.ouverte = false ;
-      //secondeBiere.lireEtiquette();
-      
-      
-        
+        //BouteilleBiere uneBiere = new BouteilleBiere() ;
+        //uneBiere.nom = "Cuvee des trolls";
+        //uneBiere.degreAlcool = 7.0 ;
+        //uneBiere.brasserie = "Dubuisson";
+        //uneBiere.ouverte = false;
+        //uneBiere.lireEtiquette();
+       
+        //BouteilleBiere secondeBiere = new BouteilleBiere() ;
+        //secondeBiere.nom = "Leffe";
+        //secondeBiere.degreAlcool = 6.6 ;
+        //secondeBiere.brasserie = "Abbaye de Leffe";
+        //secondeBiere.ouverte = false;
+        //secondeBiere.lireEtiquette();
+       
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls",7.0 ,"Dubuisson") ;
+        uneBiere.lireEtiquette();
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        autreBiere.lireEtiquette();
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("Heineken" ,5 ,"Gerard Heineken") ;
+        troisiemeBiere.lireEtiquette();
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Desperados" ,5.9 ,"Alken-Maes") ;
+        quatriemeBiere.lireEtiquette();
+        BouteilleBiere derniereBiere = new BouteilleBiere("Corona" ,4.5 ,"Grupo Modelo") ;
+        derniereBiere.lireEtiquette();
+       
+        uneBiere.decapsuler();
+        autreBiere.decapsuler();
+        quatriemeBiere.decapsuler();
+       
+        System.out.println(uneBiere);
+        System.out.println(autreBiere);
+        System.out.println(troisiemeBiere);
+        System.out.println(quatriemeBiere);
+        System.out.println(derniereBiere);
     }
+        
+    
 }
