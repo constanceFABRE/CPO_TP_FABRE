90ed7fc614b6e2803a608f632697309460ea6db7
Constance FABRE <votre_mail>
2024-10-23 17:37:06 +0200

    Création d'un nouveau projet versionné

---
+++ TP_2/TP2_convertisseurObjet_FABRE/src/main/java/com/mycompany/tp2_convertisseurobjet_fabre/Convertisseur.java
@@ -0,0 +1,16 @@
+package com.mycompany.tp2_convertisseurobjet_fabre;
+
+/*
+* ConstanceFABRE TDC
+* TP2 Ex 2
+* 23 Octobre 2024
+ */
+
+/**
+ *
+ * @author constancefabre
+ */
+public class Convertisseur {
+    
+    
+}
---
+++ TP_2/TP2_convertisseurObjet_FABRE/src/main/java/com/mycompany/tp2_convertisseurobjet_fabre/TP2_convertisseurObjet_FABRE.java
@@ -0,0 +1,19 @@
+/*
+* * ConstanceFABRE TDC
+* TP2 Ex 2
+* 23 Octobre 2024
+*/
+
+package com.mycompany.tp2_convertisseurobjet_fabre;
+
+/**
+ *
+ * @author constancefabre
+ */
+public class TP2_convertisseurObjet_FABRE {
+
+    public static void main(String[] args) {
+         
+        
+    }
+}
