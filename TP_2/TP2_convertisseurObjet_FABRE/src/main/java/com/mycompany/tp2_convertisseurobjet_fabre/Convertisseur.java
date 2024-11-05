204865f728c55aa03e7b52fb23b25b4707492a22
Constance FABRE <votre_mail>
2024-11-05 20:23:37 +0100

    Création d'un nouveau projet versionné

--- TP_2/TP2_convertisseurObjet_FABRE/src/main/java/com/mycompany/tp2_convertisseurobjet_fabre/Convertisseur.java
+++ TP_2/TP2_convertisseurObjet_FABRE/src/main/java/com/mycompany/tp2_convertisseurobjet_fabre/Convertisseur.java
@@ -11,6 +11,54 @@ package com.mycompany.tp2_convertisseurobjet_fabre;
  * @author constancefabre
  */
 public class Convertisseur {
-    
+    // Déclaration des attributs
+    int nbConversions;        // nombre de conversions effectuées par le convertisseur
+   
+    public Convertisseur () {
+        nbConversions = 0 ;
+    }
+   
+    public double CelciusVersKelvin (double tCelcius) {
+        double tKelvin;
+        tKelvin=tCelcius+273.15;
+        nbConversions++;  // Incrémente le compteur à chaque conversion
+        return tKelvin;
+    }
+   
+    public double KelvinVersCelcius (double tKelvin) {
+        double tCelcius;
+        tCelcius=tKelvin-273.15;
+        nbConversions++;  // Incrémente le compteur à chaque conversion
+        return tCelcius;
+    }
+    public double FarenheitVersCelcius (double tFarenheit) {
+        double tCelcius;
+        tCelcius=(tFarenheit-32)/1.8;
+        nbConversions++;  // Incrémente le compteur à chaque conversion
+        return tCelcius;
+    }
+    public double CelciusVersFarenheit (double tCelcius) {
+        double tFarenheit;
+        tFarenheit=(tCelcius*1.8)+32;
+        nbConversions++;  // Incrémente le compteur à chaque conversion
+        return tFarenheit;
+    }
+    public double KelvinVersFarenheit (double tKelvin) {
+        double tFarenheit;
+        tFarenheit=(tKelvin*1.8)-459.67;
+        nbConversions++;  // Incrémente le compteur à chaque conversion
+        return tFarenheit;
+    }
+    public double FarenheitVersKelvin (double tFarenheit) {
+        double tKelvin;
+        tKelvin=(tFarenheit+459.67)/1.8;
+        nbConversions++;  // Incrémente le compteur à chaque conversion
+        return tKelvin;
+    }
+   
+    @Override
+    public String toString () {
+        return "Nombre de conversions effectuées : " + nbConversions;
+    }
     
 }
--- TP_2/TP2_convertisseurObjet_FABRE/src/main/java/com/mycompany/tp2_convertisseurobjet_fabre/TP2_convertisseurObjet_FABRE.java
+++ TP_2/TP2_convertisseurObjet_FABRE/src/main/java/com/mycompany/tp2_convertisseurobjet_fabre/TP2_convertisseurObjet_FABRE.java
@@ -6,6 +6,8 @@
 
 package com.mycompany.tp2_convertisseurobjet_fabre;
 
+import java.util.Scanner;
+
 /**
  *
  * @author constancefabre
@@ -13,7 +15,77 @@ package com.mycompany.tp2_convertisseurobjet_fabre;
 public class TP2_convertisseurObjet_FABRE {
 
     public static void main(String[] args) {
-         
+     // Création d'un objet de type Convertisseur
+        Convertisseur convertisseur = new Convertisseur();
+
+        // Scanner pour lire l'entrée utilisateur
+        Scanner scanner = new Scanner(System.in);
+        int choix;
+        boolean continuer = true;
+
+        // Boucle pour afficher le menu tant que l'utilisateur ne choisit pas de quitter
+        while (continuer) {
+            System.out.println("\nMenu de conversion :");
+            System.out.println("1. Convertir Celsius vers Kelvin");
+            System.out.println("2. Convertir Kelvin vers Celsius");
+            System.out.println("3. Convertir Fahrenheit vers Celsius");
+            System.out.println("4. Convertir Celsius vers Fahrenheit");
+            System.out.println("5. Convertir Kelvin vers Fahrenheit");
+            System.out.println("6. Quitter");
+            System.out.print("Veuillez choisir une option (1-6) : ");
+            choix = scanner.nextInt();
+
+            double temp;  // Température saisie par l'utilisateur
+            double resultat;  // Résultat de la conversion
+
+            switch (choix) {
+                case 1:
+                    System.out.print("Entrez la température en Celsius : ");
+                    temp = scanner.nextDouble();
+                    resultat = convertisseur.CelciusVersKelvin(temp);
+                    System.out.println(temp + " Celsius = " + resultat + " Kelvin");
+                    break;
+
+                case 2:
+                    System.out.print("Entrez la température en Kelvin : ");
+                    temp = scanner.nextDouble();
+                    resultat = convertisseur.KelvinVersCelcius(temp);
+                    System.out.println(temp + " Kelvin = " + resultat + " Celsius");
+                    break;
+
+                case 3:
+                    System.out.print("Entrez la température en Fahrenheit : ");
+                    temp = scanner.nextDouble();
+                    resultat = convertisseur.FarenheitVersCelcius(temp);
+                    System.out.println(temp + " Fahrenheit = " + resultat + " Celsius");
+                    break;
+
+                case 4:
+                    System.out.print("Entrez la température en Celsius : ");
+                    temp = scanner.nextDouble();
+                    resultat = convertisseur.CelciusVersFarenheit(temp);
+                    System.out.println(temp + " Celsius = " + resultat + " Fahrenheit");
+                    break;
+
+                case 5:
+                    System.out.print("Entrez la température en Kelvin : ");
+                    temp = scanner.nextDouble();
+                    resultat = convertisseur.KelvinVersFarenheit(temp);
+                    System.out.println(temp + " Kelvin = " + resultat + " Fahrenheit");
+                    break;
+
+                case 6:
+                    // Afficher le nombre total de conversions avant de quitter
+                    System.out.println("Nombre total de conversions effectuées : " + convertisseur.toString());
+                    continuer = false;
+                    break;
+
+                default:
+                    System.out.println("Option invalide, veuillez réessayer.");
+            }
+        }
+        // Fermeture du scanner pour éviter les fuites de ressources
+        scanner.close();    
         
     }
 }
